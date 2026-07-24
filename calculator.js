f1e4954cd82c7c574adec36847f8db80d820ae59
Hari094A <161291929+Hari094A@users.noreply.github.com>
2026-07-24 09:56:18 +0530

    Refactor calculator functions for consistency

--- calculator.js
+++ calculator.js
@@ -1,40 +1,18 @@
-function Solve(value){
-
-    document.getElementById("ans").value += value;
-
+function Solve(val) {
+    document.getElementById("ans").value += val;
 }
 
-
-function Result(){
-
-    let x = document.getElementById("ans").value;
-
-    try{
-
-        document.getElementById("ans").value = eval(x);
-
-    }
-
-    catch{
-
-        document.getElementById("ans").value = "Error";
-
-    }
-
+function Result() {
+    var x = document.getElementById("ans").value;
+    var y = eval(x);
+    document.getElementById("ans").value = y;
 }
 
-
-function Clear(){
-
+function Clear() {
     document.getElementById("ans").value = "";
-
 }
 
-
-function Back(){
-
-    let value = document.getElementById("ans").value;
-
-    document.getElementById("ans").value = value.slice(0,-1);
-
+function Back() {
+    var x = document.getElementById("ans").value;
+    document.getElementById("ans").value = x.slice(0, -1);
 }
